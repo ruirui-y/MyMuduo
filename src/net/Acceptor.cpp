2d91ca9525a33a69baaccbc6c0dfb176f6398690
13083361602 <3265868153@qq.com>
2026-03-12 10:52:11 +0800

    处理文件描述符耗尽

--- src/net/Acceptor.cpp
+++ src/net/Acceptor.cpp
@@ -4,12 +4,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 Acceptor::Acceptor(EventLoop* loop, const std::string& ip, uint16_t port)
 	:loop_(loop),
 	accept_socket_(::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0)),
 	accept_channel_(loop, accept_socket_.fd()),
-	listening_(false)
+	listening_(false),
+    idle_fd_(::open("/dev/null", O_RDONLY | O_CLOEXEC))
 {
     accept_socket_.BindAddress(ip, port);
 	accept_channel_.SetReadCallback(bind(&Acceptor::HandleRead, this));
@@ -44,6 +46,18 @@ void Acceptor::HandleRead()
     }
     else
     {
-        LOG_ERROR << "Acceptor::HandleRead error";
+        // 如果文件描述符耗尽
+        if(errno == EMFILE)
+        {
+            LOG_ERROR << "Acceptor::HandleRead error: EMFILE, fd limit reached!";
+            ::close(idle_fd_);                                                              // 1. 扔掉占坑 fd，腾出一个名额
+            idle_fd_ = ::accept(accept_socket_.fd(), NULL, NULL);                           // 2. 接收这个累赘
+            ::close(idle_fd_);                                                              // 3. 优雅地挂断它
+            idle_fd_ = ::open("/dev/null", O_RDONLY | O_CLOEXEC);                           // 4. 重新占坑，以备下次使用
+        }
+        else
+        {
+            LOG_ERROR << "Acceptor::HandleRead error";
+        }
     }
 }
